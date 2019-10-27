71d718c449f942d4ffc66be3ed30d083fd25984e
rksyravi <ravi.singh2@kelltontech.com>
2019-10-27 18:15:00 +0530

    time dynamic

--- site_location/src/SiteLocationService.php
+++ site_location/src/SiteLocationService.php
@@ -3,6 +3,7 @@
 namespace Drupal\site_location;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\PageCache\ResponsePolicyInterface;
 
 /**
  * Class SiteLocationService.
@@ -22,16 +23,27 @@ class SiteLocationService implements SiteLocationInterface {
    * @var \Drupal\Core\Datetime\DateFormatterInterface
    */
   protected $dateFormatter;
+  
+  /**
+   * Drupal\Core\PageCache\ResponsePolicyInterface definition.
+   *
+   * @var \Drupal\Core\PageCache\ResponsePolicyInterface
+   */
+  protected $pageCacheKillSwitch;
 
   /**
    * Constructs a new DefaultService object.
    */
-  public function __construct(TimeInterface $datetime_time, DateFormatterInterface $date_formatter) {
+  public function __construct(TimeInterface $datetime_time, DateFormatterInterface $date_formatter, ResponsePolicyInterface $page_cache_kill_switch) {
     $this->datetimeTime = $datetime_time;
     $this->dateFormatter = $date_formatter;
+    $this->pageCacheKillSwitch = $page_cache_kill_switch;
   }
   
   public function CurrentDateTime($timezone) {
+    if (empty(\Drupal::currentUser()->id())) {
+      $this->pageCacheKillSwitch->trigger();
+    }
     return ['#markup'=> $this->dateFormatter->format($this->datetimeTime->getCurrentTime(), 'custom', 'dS M Y - h:i A', $timezone)];
   }
 
